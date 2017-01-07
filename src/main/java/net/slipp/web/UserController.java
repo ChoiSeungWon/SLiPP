53fb7514b7b6e45a1310d1ee60e1f07d3b39e3e2
charles <b088081@gmail.com>
2017-01-07 14:49:10 +0900

    로그인 기능 추가

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -3,6 +3,8 @@ package net.slipp.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,13 +25,30 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-//	@PostMapping("/create")
-//	public String create(User user){
-//		System.out.println(user);
-//		users.add(user);
-//		userRepository.save(user);
-//		return "redirect:/list";
-//	}
+	@GetMapping("/loginForm")
+	public String loginForm(){
+		return "user/login";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId,String password,HttpSession session){
+		User user = userRepository.findByUserId(userId);
+		if(user == null){
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())){
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		
+		return "redirect:/";
+	}
+	
 	@GetMapping("/form")
 	public String form(Model model){
 		model.addAttribute("users", userRepository.findAll());
