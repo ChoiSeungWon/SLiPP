d5646f3d48ee2031cbe2999e7fa4b540fe36e0b4
charles <b088081@gmail.com>
2017-01-07 19:00:39 +0900

    자기 자신의 한해 개인정보 수정 기능하도록 구현

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -13,13 +13,21 @@ public class User {
 	@GeneratedValue // 자동 1 증가 
 	private Long id;
 	
-	@Column(nullable=false, length=20)
+	@Column(nullable=false, length=20, unique=true)
 	private String userId;	
 	
 	private String password;
 	private String name;
 	private String email;
 	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
 	public String getUserId() {
 		return userId;
 	}
--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -44,14 +44,14 @@ public class UserController {
 		}
 		
 		System.out.println("Login Success!");
-		session.setAttribute("user", user);
+		session.setAttribute("sessionedUser", user);
 		
 		return "redirect:/";
 	}
 	
 	@GetMapping("/logout")
 	public String logout(HttpSession session){
-		session.removeAttribute("user");
+		session.removeAttribute("sessionedUser");
 		
 		return "redirect:/";
 	}
@@ -77,7 +77,17 @@ public class UserController {
 	}
 	
 	@GetMapping("/{id}/form")
-	public String updateForm(@PathVariable Long id, Model model){
+	public String updateForm(@PathVariable Long id, Model model, HttpSession session){
+		Object tempUser = session.getAttribute("sessionedUser");
+		if(tempUser == null){
+			return "redirect:/users/loginForm";
+		}
+		
+		User sessionedUser = (User)tempUser;
+		if(!id.equals(sessionedUser.getId())){
+			throw new IllegalStateException("you can't update the anther user.");
+		}
+		
 		
 		User user = userRepository.findOne(id);
 		System.out.println(user.toString());
@@ -86,9 +96,19 @@ public class UserController {
 	}
 	
 	@PutMapping("/{id}")
-	public String update(@PathVariable Long id,User newUser){
+	public String update(@PathVariable Long id,User updatedUser, HttpSession session){
+		Object tempUser = session.getAttribute("sessionedUser");
+		if(tempUser == null){
+			return "redirect:/users/loginForm";
+		}
+		
+		User sessionedUser = (User)tempUser;
+		if(!id.equals(sessionedUser.getId())){
+			throw new IllegalStateException("you can't update the anther user.");
+		}
+		
 		User user = userRepository.findOne(id);
-		user.update(newUser);
+		user.update(updatedUser);
 		userRepository.save(user);
 		
 		return "redirect:/users";
