b37cdce3e152923b7df8efae81f5f41af885c799
charles <b088081@gmail.com>
2017-01-07 20:10:58 +0900

    중복 제거, clean code, 쿼리 보기 설정

--- src/main/java/net/slipp/domain/User.java
+++ src/main/java/net/slipp/domain/User.java
@@ -62,6 +62,19 @@ public class User {
 		this.name = newUser.name;
 		this.email = newUser.email;
 	}
-	
+	public boolean matchId(Long newId){
+		if (newId == null){
+			return false;
+		}
+		
+		return newId.equals(id);
+	}
+	public boolean matchPassword(String newPassword){
+		if (newPassword == null){
+			return false;
+		}
+		
+		return newPassword.equals(password);
+	}
 	
 }
